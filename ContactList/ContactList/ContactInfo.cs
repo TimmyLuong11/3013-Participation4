149a1bcb3630a55ca056520e6d47458384ce6413
TimmyLuong11 <Timothy.luong@ou.edu>
2021-09-23 15:40:38 -0500

    Finished Code

---
+++ ContactList/ContactList/ContactInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactList
+{
+    class ContactInfo
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+
+        public ContactInfo()
+        {
+            Id = string.Empty;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            Photo = string.Empty;
+        }
+
+        public ContactInfo(string line)
+        {
+            string[] pieces = line.Split('|');
+            Id = pieces[0];
+            FirstName = pieces[1];
+            LastName = pieces[2];
+            Email = pieces[3];
+            Photo = pieces[4];
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+    }
+}
---
+++ ContactList/ContactList/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<ContactInfo> contacts = new List<ContactInfo>();
+            var lines = File.ReadAllLines("contacts.txt");
+            foreach (var item in lines)
+            {
+                contacts.Add(new ContactInfo(item));
+            }
+
+            for (int i = 1; i < contacts.Count; i++)
+            {
+                listBoxContact.Items.Add(contacts[i]);
+            }
+        }
+
+        private void buttonDetail_Click(object sender, RoutedEventArgs e)
+        {
+            ContactInfo cInfo = (ContactInfo)listBoxContact.SelectedItem;
+            txtFirst.Text = cInfo.FirstName;
+            txtLast.Text = cInfo.LastName;
+            txtEmail.Text = cInfo.Email;
+            var uri = new Uri(cInfo.Photo);
+            var img = new BitmapImage(uri);
+            imgBox.Source = img;
+        }
+    }
+}
